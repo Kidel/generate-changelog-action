b25309eed511ca690862b372699c9d0efec2a66f
Gaetano Bonofiglio <Kidel@users.noreply.github.com>
2020-08-05 18:22:47 +0200

    use conventional-changelog instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,8 @@
 
 git clone --quiet -b $BRANCH --single-branch https://github.com/$REPO &> /dev/null
 
-changelog=$(generate-changelog $* --file -)
+changelog=$(conventional-changelog $* -s)
 
 echo $changelog
-
-changelog="${changelog//'%'/'%25'}"
-changelog="${changelog//$'\n'/'%0A'}"
-changelog="${changelog//$'\r'/'%0D'}"
 
 echo "::set-output name=changelog::$changelog"
